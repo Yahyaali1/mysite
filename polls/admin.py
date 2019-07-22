3a48c98c0385d956f5b1b19df2e2805324be6025
Yahya Ali <yahya.ali@tintash.com>
2019-07-22 10:46:28 +0500

    Added extra info to be displayed in Admin page

--- polls/admin.py
+++ polls/admin.py
@@ -1,6 +1,21 @@
 from django.contrib import admin
 from .models import *
 # Register your models here.
-admin.site.register(Question)
+
 admin.site.register(Choice)
 admin.site.register(People)
+
+class ChoiceInLine(admin.TabularInline):
+    model = Choice
+    extra = 2
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ['question_text','pub_date']
+    list_filter = ['question_text']
+    fieldsets =[
+        (None,{'fields':['question_text']}),
+        ('Date Info',{'fields':['pub_date']})
+    ]
+    inlines = [ChoiceInLine]
+
+
+admin.site.register(Question,QuestionAdmin)
--- polls/views.py
+++ polls/views.py
@@ -29,7 +29,6 @@ class ResultView(generic.DetailView):
 
 def vote(request,question_id):
 	question = get_object_or_404(Question,pk = question_id)
-	new = variables
 	try:
 		selected_choice = question.choice_set.get(pk = request.POST['choice'])
 	except (KeyError , Choice.DoesNotExist):
